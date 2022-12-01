cf1d6cb3bb74c9398f926ebb03264a4c6c49ba9f
Gabriel Lima <108353430+GabrielCcr25@users.noreply.github.com>
2022-12-01 20:49:13 -0300

    Add files via upload

---
+++ Publicacao.java
@@ -0,0 +1,10 @@
+package br.com.newton.Questao2ProvaFinal;
+
+public interface Publicacao {
+    public abstract boolean buscarTitulo(String titulo);
+    public abstract void vizualizarDetalhes();
+
+
+
+
+}
